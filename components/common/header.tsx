416941a2cd35dfdb3c7ca076ef683fc453288993
AyushDeuja <ayushdeuja11@gmail.com>
2025-12-29 21:09:55 +0545

    auth done

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -7,6 +7,15 @@ import {
   UserIcon,
 } from "lucide-react";
 import { Button } from "../ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  SignUp,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Logo = () => {
   return (
@@ -22,7 +31,6 @@ const Logo = () => {
 };
 
 export default function Header() {
-  const isSignedIn = true; // Replace with actual authentication logic
   return (
     <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="wrapper px-12">
@@ -46,25 +54,21 @@ export default function Header() {
           </nav>
 
           <div className="flex items-center gap-3">
-            {isSignedIn ? (
-              <>
-                <Button asChild>
-                  <Link href="/submit">
-                    <SparklesIcon className="size-4" />
-                    Submit Project
-                  </Link>
-                </Button>
-                {/* clerk user */}
-                <Button variant="ghost">
-                  <UserIcon className="size-4" />
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button variant="ghost">Sign In</Button>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
                 <Button>Sign Up</Button>
-              </>
-            )}
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <Button asChild>
+                <Link href="/submit">
+                  <SparklesIcon className="size-4" />
+                  Submit Project
+                </Link>
+              </Button>
+              <UserButton />
+            </SignedIn>
           </div>
         </div>
       </div>
